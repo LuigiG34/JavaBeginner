0256dda0b4f8d3ea184e7c4bee53e6317e87e283
Luigi Gandemer <luigigandemer6@gmail.com>
2026-09-23 15:38:33 +0200

    Create an employee, a manager and calculate their total salaries

--- exercice_2/Main.java
+++ exercice_2/Main.java
@@ -1,7 +1,16 @@
 package exercice_2;
 
+import exercice_2.model.Employee;
+import exercice_2.model.Manager;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Employee employee = new Employee("John Doe", 50000L);
+        Manager manager = new Manager("Jane Smith", 70000L, 20000L);
+
+        System.out.println("Employee Payment: " + employee.calculatePayment());
+        System.out.println("Manager Payment: " + manager.calculatePayment());
     }
 }
