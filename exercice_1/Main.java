c33cd49b80e36eefde0ac0313144ba007878baaa
Luigi Gandemer <luigigandemer6@gmail.com>
2026-09-23 14:35:06 +0200

    Create 2 products and apply a discount

--- exercice_1/Main.java
+++ exercice_1/Main.java
@@ -3,5 +3,25 @@ package exercice_1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        // Create 2 products
+        Product product1 = new Product(1L, "Product 1", 950L, true);
+        System.out.println("---- First product created ----");
+        System.out.println("Name: " + product1.getName());
+        System.out.println("Price: " + product1.getFormattedPrice() + "€");
+        System.out.println("Available: " + product1.isAvailable());
+
+
+        Product product2 = new Product(2L, "Product 2", 1500L, false);
+        System.out.println("---- Second product created ----");
+        System.out.println("Name: " + product2.getName());
+        System.out.println("Price: " + product2.getFormattedPrice() + "€");
+        System.out.println("Available: " + product2.isAvailable());
+
+        // Apply a discount to the first product
+        product1.applyDiscount(12.0);
+        System.out.println("---- After applying 12% discount to the first product ----");
+        System.out.println("Name: " + product1.getName());
+        System.out.println("Price: " + product1.getFormattedPrice() + "€"); 
     }
 }
