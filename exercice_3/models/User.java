cf99016b57f191a174bb2d2c5ecd0a57c067e1c5
Luigi Gandemer <luigigandemer6@gmail.com>
2026-09-24 15:34:34 +0200

    User entity created

---
+++ exercice_3/models/User.java
@@ -0,0 +1,23 @@
+package exercice_3.models;
+
+public class User {
+    private Long id;
+    private String email;
+
+    public User(Long id, String email) {
+        this.id = id;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
