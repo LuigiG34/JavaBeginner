d6f75b35c8077109bf1569d743d6f4b0195ec8bc
Luigi Gandemer <luigigandemer6@gmail.com>
2026-09-25 16:53:35 +0200

    Test finding a product by id with and without valid id to throw custom exception

--- exercice_5/Main.java
+++ exercice_5/Main.java
@@ -1,7 +1,41 @@
 package exercice_5;
 
+import exercice_5.model.Product;
+import exercice_5.repository.ProductRepository;
+import exercice_5.exception.ProductNotFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        // Create a list of 5 products
+        List<Product> products = new ArrayList<>();
+        products.add(new Product(1L, "Product 1", 10.0));
+        products.add(new Product(2L, "Product 2", 20.0));
+        products.add(new Product(3L, "Product 3", 30.0));
+        products.add(new Product(4L, "Product 4", 40.0));
+        products.add(new Product(5L, "Product 5", 50.0));
+
+        // Create a ProductRepository with the list of products
+        ProductRepository productRepository = new ProductRepository(products);
+
+        // Test getProductById with a valid id
+        try {
+            Product product = productRepository.getProductById(3L);
+            System.out.println("Product found: " + product.getName() + " - " + product.getPrice());
+        } catch (ProductNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        // Test getProductById with an invalid id
+        try {
+            Product product = productRepository.getProductById(6L);
+            System.out.println("Product found: " + product.getName() + " - " + product.getPrice());
+        } catch (ProductNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
